448a3abb3b746d451dbf0a9e28f3685bdf7a1ad8
jillzhou88 <jillzhou88@gmail.com>
2018-08-16 11:26:19 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { Grid, Row, Navbar } from 'react-bootstrap';
-import data from './assets/data.json';
-import './App.css';
+import React, { Component } from "react";
+import { Grid, Row, Navbar } from "react-bootstrap";
+import data from "./assets/data.json";
+import classes from "./App.css";
 
-import Card from './Components/Card/Card';
-import List from './Components/List/List';
+import Card from "./Components/Card/Card";
 
 class App extends Component {
   state = {
     recommendations: {},
     mylist: {}
-  }
+  };
 
   componentWillMount() {
-    let recommendations = {}, mylist = {};
-    data.recommendations.forEach((el) => {
+    let recommendations = {},
+      mylist = {};
+    data.recommendations.forEach(el => {
       recommendations[el.id] = el;
     });
-    data.mylist.forEach((el) => {
+    data.mylist.forEach(el => {
       mylist[el.id] = el;
     });
     this.setState({
@@ -26,41 +26,77 @@ class App extends Component {
     });
   }
 
-  removeMylist = (id) => {
+  removeMylist = id => {
     let mylist = JSON.parse(JSON.stringify(this.state.mylist));
+    let recommendations = JSON.parse(JSON.stringify(this.state.recommendations));
+    recommendations[id] = mylist[id];
     delete mylist[id];
-    this.setState({mylist: mylist});
-  }
+    this.setState({ mylist: mylist, recommendations: recommendations });
+  };
 
-  addMylist = (id) => {
+  addMylist = id => {
     let mylist = JSON.parse(JSON.stringify(this.state.mylist));
-    mylist[id] = this.state.recommendations[id];
-    this.setState({mylist: mylist});
-  }
+    let recommendations = JSON.parse(JSON.stringify(this.state.recommendations));
+    mylist[id] = recommendations[id];
+    delete recommendations[id];
+    this.setState({ mylist: mylist, recommendations: recommendations });
+  };
 
   render() {
     let mylist;
     mylist = Object.entries(this.state.mylist).map(([key, value]) => {
       return (
-        <Card item={value} key={value.title+key} button="Remove" click={this.removeMylist}/>
+        <Card
+          item={value}
+          key={value.title + key}
+          button="Remove"
+          click={this.removeMylist}
+        />
       );
     });
 
     let recommendations;
-    recommendations = Object.entries(this.state.recommendations).map(([key, value]) => {
-      return (
-        <Card item={value} key={value.title+key} button="Add" click={this.addMylist}/>
-      );
-    });
-    
-    return <Grid>
-        <Row>My list:</Row>
-        <Row>{mylist}</Row>
-        <hr />
-        <Row>Recommendations:</Row>
-        <Row>{recommendations}</Row>
-        <Navbar fixedBottom><List list={this.state.mylist} /></Navbar>
-      </Grid>;
+    recommendations = Object.entries(this.state.recommendations).map(
+      ([key, value]) => {
+        return (
+          <Card
+            item={value}
+            key={value.title + key}
+            button="Add"
+            click={this.addMylist}
+          />
+        );
+      }
+    );
+
+    return (
+      <div className="App">
+        <header className="header">
+          <svg
+            viewBox="0 0 111 30"
+            id="netflix-logo"
+            width="100%"
+            height="100%"
+          >
+            {" "}
+            <path d="M105.062 14.28L111 30c-1.75-.25-3.499-.563-5.28-.845l-3.345-8.686-3.437 7.969c-1.687-.282-3.344-.376-5.031-.595l6.031-13.75L94.468 0h5.063l3.062 7.874L105.875 0h5.124l-5.937 14.28zM90.47 0h-4.594v27.25c1.5.094 3.062.156 4.594.343V0zm-8.563 26.937c-4.187-.281-8.375-.53-12.656-.625V0h4.687v21.875c2.688.062 5.375.28 7.969.405v4.657zM64.25 10.657v4.687h-6.406V26H53.22V0h13.125v4.687h-8.5v5.97h6.406zm-18.906-5.97V26.25c-1.563 0-3.156 0-4.688.062V4.687h-4.844V0h14.406v4.687h-4.874zM30.75 15.593c-2.062 0-4.5 0-6.25.095v6.968c2.75-.188 5.5-.406 8.281-.5v4.5l-12.968 1.032V0H32.78v4.687H24.5V11c1.813 0 4.594-.094 6.25-.094v4.688zM4.78 12.968v16.375C3.094 29.531 1.593 29.75 0 30V0h4.469l6.093 17.032V0h4.688v28.062c-1.656.282-3.344.376-5.125.625L4.78 12.968z" />
+          </svg>
+          <p>Netflix like page</p>
+        </header>
+        <Grid >
+        <div className={classes.list}>
+          <Row><span className={classes.listHeader}>My list:</span></Row>
+          <hr />
+          <Row>{mylist}</Row>
+          </div>
+          <div className={classes.list}>
+          <Row><span className={classes.listHeader}>Recommendations:</span></Row>
+          <hr />
+          <Row>{recommendations}</Row>
+          </div>
+        </Grid>
+      </div>
+    );
   }
 }
 
